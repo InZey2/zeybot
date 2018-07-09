945fd7a1565dcc15c2b942259455b9879f674eeb
InZey2 <40923504+InZey2@users.noreply.github.com>
2018-07-09 12:54:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,14 +1,1 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-client.login('NDY1NjI1MDM0MDE5MzA3NTIw.DiQOoQ.Zc3A0qkgVNc4u-acZsaHJD5vSl4');
